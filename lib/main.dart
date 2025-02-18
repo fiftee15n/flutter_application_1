a1070032cf166e1edf586f6f806524ba18fe5151
fiftee15n <tamaljalam15@gmail.com>
2025-02-18 09:54:20 +0600

    Working with row

--- lib/main.dart
+++ lib/main.dart
@@ -9,34 +9,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text('Simple Flutter Container')),
-        body: Center(
-          child: Container(
-            width: double.infinity, // To fill the width of the screen
-            height: double.infinity, // To fill the height of the screen
-            decoration: BoxDecoration(
-              color: const Color.fromARGB(255, 197, 239, 129), // Background color of the screen
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(30), // Rounded corners on top left
-                topRight: Radius.circular(30), // Rounded corners on top right
+        appBar: AppBar(title: Text('Flutter Project Tutorial')),
+        body: Row(
+          children: [
+            Text(
+              "Hi,",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 30,
               ),
             ),
-            child: Center(
-              child: Container(
-                width: 200, // Width of the inner container
-                height: 100, // Height of the inner container
-                color: const Color.fromARGB(255, 50, 100, 0), // Background color of the inner container
-                alignment: Alignment.center, // Align the child widget inside
-                child: Text(
-                  'Hello Flutter!',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 255, 255, 255), // Text color
-                    fontSize: 20, // Text size
-                  ),
-                ),
+            Text(
+              "This is ",
+              style: TextStyle(
+                color: Colors.blue,
+                fontSize: 20,
               ),
             ),
-          ),
+            Text(
+              "Flutter",
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 45,
+              ),
+            )
+          ],
         ),
       ),
     );
