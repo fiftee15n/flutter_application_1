c6fc6c7688f28ab5bc0859c6702a911e709fe2da
fiftee15n <tamaljalam15@gmail.com>
2025-02-18 10:48:13 +0600

    Work with row Alignmnet

--- lib/main.dart
+++ lib/main.dart
@@ -11,38 +11,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter Project Tutorial')),
-        body: Row(
-          children: [
-            Icon(
-              Icons.star,
-              size: 50,
-              color:const Color.fromARGB(255, 7, 7, 7),
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-              color:const Color.fromARGB(255, 7, 7, 7),
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-              color:const Color.fromARGB(255, 7, 7, 7),
-            ),
-            Icon(
-              Icons.star,
-              size: 50,
-              color:const Color.fromARGB(255, 7, 7, 7),
-            ),
-            Icon(
-              Icons.star_outline,
-              size: 50,
-              color:const Color.fromARGB(255, 0, 0, 0),
+        body: Container(
+          height: double.infinity,
+          child: Row(
+
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.home,
+                size: 30,
+                color:const Color.fromARGB(255, 254, 4, 4),
+              ),
+              Icon(
+                Icons.shop,
+                size: 30,
+                color:const Color.fromARGB(255, 5, 81, 244),
+              ),
+              Icon(
+                Icons.star,
+                size: 30,
+                color:const Color.fromARGB(255, 236, 0, 248),
+              ),
               
-            ),
-            
-            
-            
-          ],
+                
+              
+              
+              
+              
+            ],
+          ),
         ),
       ),
     );
